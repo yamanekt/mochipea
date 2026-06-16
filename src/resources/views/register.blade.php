4b4f937441717f62e9264c91ed475c8103b7acf9
kakeru <kakeru.181115@gmail.com>
2026-06-16 16:01:15 +0900

    register

--- src/resources/views/register.blade.php
+++ src/resources/views/register.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>新規登録</title>
     <link rel="stylesheet" href="{{ asset('css/register.css') }}">
 </head>
@@ -11,35 +12,35 @@
 
     <div class="container">
         <h1>新規登録</h1>
-        <h2>アカウント作成</h2>
+        <h2></h2>
 
-        <form action="{{ url('/register') }}" method="post">
-            @if ($errors->any())
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li style="color:red">{{ $error }}</li>
-                    @endforeach
-                </ul>
-            @endif
+<form action="{{ url('/register') }}" method="post">
+    @csrf
 
-            <form action="{{ url('/register') }}" method="get">
-                @csrf
-                <label>ユーザー名 *</label>
-                <input type="text" name="name" required>
+    @if ($errors->any())
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li style="color:red">{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+<label for="name">ユーザー名 *</label>
+<input type="text" id="name" name="name" autocomplete="username" required>
 
-                <label>Eメール *</label>
-                <input type="email" name="email" required>
+<label for="email">Eメール *</label>
+<input type="email" id="email" name="email" autocomplete="email" required>
 
-                <label>パスワード *</label>
-                <input type="password" name="password" required>
+<label for="password">パスワード *</label>
+<input type="password" id="password" name="password" autocomplete="new-password" required>
 
-                <label>パスワード確認 *</label>
-                <input type="password" name="password_confirmation" required>
-
-                <button type="submit" class="main-btn">アカウント作成</button>
-            </form>
+<label for="password_confirmation">パスワード確認 *</label>
+<input type="password" id="password_confirmation" name="password_confirmation" autocomplete="new-password" required>
+    <button type="submit">アカウント作成</button>
+</form>
     </div>
 
 </body>
 
 </html>
+
+
