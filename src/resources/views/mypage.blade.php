df9414d4721e74c51898791c807ffec9d7af63dd
yamanekt <yamakei0929neta@gmail.com>
2026-09-04 12:13:11 +0900

    パスワード変更削除

--- src/resources/views/mypage.blade.php
+++ src/resources/views/mypage.blade.php
@@ -59,13 +59,13 @@
                 </span>
             </a>
 
-            <a href="{{ route('account.password') }}" class="row">
+            {{-- <a href="{{ route('account.password') }}" class="row">
                 <span class="row-label">パスワード</span>
                 <span class="row-value">
                     <span class="row-accent">変更する</span>
                     <span class="row-chevron" aria-hidden="true">›</span>
                 </span>
-            </a>
+            </a> --}}
         </div>
 
         <div class="card">
